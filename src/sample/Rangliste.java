d080795bbe22b8f52e71f6d8d9bf39cd054d2b27
fischer <franz.fischer@futuretec-systems.de>
2016-05-16 14:58:33 +0200

    add Rangliste and RanglisteInterface

---
+++ src/sample/Rangliste.java
@@ -0,0 +1,26 @@
+package sample;
+
+/**
+ * Created by franz on 16.05.16.
+ */
+public class Rangliste implements RanglisteInterface {
+    @Override
+    public void addPlayer() {
+
+    }
+
+    @Override
+    public void addTeam() {
+
+    }
+
+    @Override
+    public Rangliste sortList() {
+        return null;
+    }
+
+    @Override
+    public Rangliste getListing() {
+        return null;
+    }
+}
---
+++ src/sample/RanglisteInterface.java
@@ -0,0 +1,18 @@
+package sample;
+
+
+
+/**
+ * Created by franz on 16.05.16.
+ */
+
+
+public interface RanglisteInterface {
+
+
+void addPlayer(Player player);
+void addTeam(Team team);
+Rangliste sortList(int way);
+Rangliste getListing(int way);
+
+}
