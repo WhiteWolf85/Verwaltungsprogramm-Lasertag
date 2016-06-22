3467337f3efe02bda474c4effed52b715358dfd3
franz <franzfischer1985@gmail.com>
2016-06-22 21:10:30 +0200

    update listController

--- src/ListController.java
+++ src/ListController.java
@@ -13,90 +13,52 @@ import java.util.ArrayList;
 public class ListController {
 
     @FXML
-    private TableView listTeam;
+    private TableView listTeam, listPlayer;
     @FXML
-    private TableColumn<Team,String> teamname;
+    private TableColumn<Team,String> teamname, teamscore;
     @FXML
-    private TableColumn<Team,String>teamscore;
+    private TableColumn<Spieler,String> player, playerScore;
 
-    //Testwerte für Rangliste
-
-/*
-    private Team beamforce = new Team("Beamforce");
-    private Team btb = new Team("BTB");
-    private Spieler whiteWolf = new Spieler();
-    private Spieler bloodyMary = new Spieler();
-    private Spieler reitermaniac=new Spieler();
-    private Spieler pg = new Spieler();
-    private Spieler tine = new Spieler();
     private Rangliste sort=new Rangliste();
-*/
-    private void setValues()
-    {
-
-/*
-
-        beamforce.setTeamName("Beamforce ONE");
-        btb.setTeamName("BTB");
-
-        whiteWolf.age = 30;
-        whiteWolf.name = "Franz";
-        whiteWolf.status = "aktiv";
-        whiteWolf.nickName = "White Wolf";
-        whiteWolf.setRole("Mittelfeld");
-        whiteWolf.score = 9001;               //over 9000 ;-)
-
-        bloodyMary.age = 20;
-        bloodyMary.name = "Anna";
-        bloodyMary.status = "aktiv";
-        bloodyMary.nickName = "Bloody Mary";
-        bloodyMary.setRole("Runner");
-        bloodyMary.score = 689;
-
-        reitermaniac.age = 20;
-        reitermaniac.name = "Tom";
-        reitermaniac.status = "aktiv";
-        reitermaniac.nickName = "Reitermaniac";
-        reitermaniac.setRole("Runner");
-        reitermaniac.score = 800;
-
-        pg.age = 23;
-        pg.name = "PG";
-        pg.status = "aktiv";
-        pg.nickName = "PG";
-        pg.setRole("Runner");
-        pg.score = -30;
-
-        tine.age = 21;
-        tine.name = "Tine";
-        tine.status = "verletzt";
-        tine.nickName = "Tine";
-        tine.setRole("Mittelfeld");
-        tine.score = 50;
-*/
-    }
-    //Ende Testwerte
 
     public void initialize(){
         this.fillList();
     }
 
     public void fillList(){
-        setValues();
-        //ArrayList<Sortable> list1=(ArrayList<Sortable>) Controller.globalTeams;
-        //list1.add(beamforce);
-        //list1.add(btb);
-        Rangliste sort=new Rangliste();
-
-        //ArrayList<Team> sortedTeam=(ArrayList<Team>)sort.getSortedList(list1);
         ObservableList<Team> ol= FXCollections.observableArrayList(Main.globalTeams);
 
         teamname.setCellValueFactory(new PropertyValueFactory<Team,String>("teamName"));
         teamscore.setCellValueFactory(new PropertyValueFactory<Team,String>("teamScore"));
 
         listTeam.setItems(ol);
+    }
+
+    public void getPlayers(){
+        Team team=(Team)listTeam.getSelectionModel().getSelectedItem();
+        Spieler[] players=team.getSpieler();
+
+        ArrayList<Spieler> playerList=new ArrayList<Spieler>();
+
+        //weil .getSpieler ein Array und keine ArrayList zurückgibt
+        for(Spieler var:players){
+            playerList.add(var);
+        }
+
+        ObservableList<Spieler> ol= FXCollections.observableArrayList(playerList);
+
+        player.setCellValueFactory(new PropertyValueFactory<Spieler,String>("nickName"));
+        playerScore.setCellValueFactory(new PropertyValueFactory<Spieler,String>("score"));
 
 
+        listPlayer.setItems(ol);
+    }
+
+    public void getAllPlayers(){
+        ObservableList<Spieler> ol= FXCollections.observableArrayList(Main.globalPlayers);
+        player.setCellValueFactory(new PropertyValueFactory<Spieler,String>("nickName"));
+        playerScore.setCellValueFactory(new PropertyValueFactory<Spieler,String>("score"));
+        listPlayer.setItems(ol);
     }
 
 }
