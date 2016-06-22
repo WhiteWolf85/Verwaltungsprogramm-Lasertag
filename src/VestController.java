120287cbd174cc87e336c2db2914f505a14b9693
Anastasia Albrecht <anastasia.kb.albrecht@gmx.de>
2016-06-22 19:10:11 +0200

    vest.fxml

--- src/VestController.java
+++ src/VestController.java
@@ -1,6 +1,9 @@
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 
 /**
@@ -9,29 +12,101 @@ import javafx.scene.control.ListView;
 public class VestController {
 
     @FXML
-    private ListView vest1;
-    @FXML
-    private ListView vest2;
-    @FXML
-    private ListView vest3;
-    @FXML
-    private ListView vest4;
-    @FXML
-    private ListView vest5;
-    @FXML
-    private ListView vest6;
-    @FXML
-    private ListView vest7;
-    @FXML
-    private ListView vest8;
-    @FXML
-    private ListView vest9;
-    @FXML
-    private ListView vest10;
-    @FXML
-    private ListView vest11;
-    @FXML
-    private ListView vest12;
+    public Label label1, label2, label3, label4, label5;
+    public Button vest1, vest2, vest3, vest4, vest5, vest6, vest7, vest8, vest9, vest10, vest11, vest12;
 
+    public void reactToButtonClickV1(ActionEvent actionEvent) {
+        label1.setText("15");
+        label2.setText("11");
+        label3.setText("42");
+        label4.setText("57.69");
+        label5.setText("ready");
+    }
 
-}
+    public void reactToButtonClickV2(ActionEvent actionEvent) {
+        label1.setText("15");
+        label2.setText("11");
+        label3.setText("42");
+        label4.setText("57.69");
+        label5.setText("ready");
+    }
+    public void reactToButtonClickV3(ActionEvent actionEvent) {
+        label1.setText("15");
+        label2.setText("11");
+        label3.setText("42");
+        label4.setText("57.69");
+        label5.setText("ready");
+    }
+
+    public void reactToButtonClickV4(ActionEvent actionEvent) {
+        label1.setText("15");
+        label2.setText("11");
+        label3.setText("42");
+        label4.setText("57.69");
+        label5.setText("ready");
+    }
+
+    public void reactToButtonClickV5(ActionEvent actionEvent) {
+        label1.setText("15");
+        label2.setText("11");
+        label3.setText("42");
+        label4.setText("57.69");
+        label5.setText("ready");
+    }
+
+    public void reactToButtonClickV6(ActionEvent actionEvent) {
+        label1.setText("15");
+        label2.setText("11");
+        label3.setText("42");
+        label4.setText("57.69");
+        label5.setText("ready");
+    }
+
+    public void reactToButtonClickV7(ActionEvent actionEvent) {
+        label1.setText("15");
+        label2.setText("11");
+        label3.setText("42");
+        label4.setText("57.69");
+        label5.setText("ready");
+    }
+
+    public void reactToButtonClickV8(ActionEvent actionEvent) {
+        label1.setText("15");
+        label2.setText("11");
+        label3.setText("42");
+        label4.setText("57.69");
+        label5.setText("ready");
+    }
+
+    public void reactToButtonClickV9(ActionEvent actionEvent) {
+        label1.setText("15");
+        label2.setText("11");
+        label3.setText("42");
+        label4.setText("57.69");
+        label5.setText("ready");
+    }
+    public void reactToButtonClickV10(ActionEvent actionEvent) {
+        label1.setText("15");
+        label2.setText("11");
+        label3.setText("42");
+        label4.setText("57.69");
+        label5.setText("ready");
+    }
+
+    public void reactToButtonClickV11(ActionEvent actionEvent) {
+        label1.setText("15");
+        label2.setText("11");
+        label3.setText("42");
+        label4.setText("57.69");
+        label5.setText("ready");
+    }
+
+    public void reactToButtonClickV12(ActionEvent actionEvent) {
+        label1.setText("15");
+        label2.setText("11");
+        label3.setText("42");
+        label4.setText("57.69");
+        label5.setText("ready");
+    }
+
+    }
