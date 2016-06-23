43d931afbc6f41f57f70f104c76ef6ca4d74f0c1
Reitermaniac <thomasrandl@web.de>
2016-06-23 14:20:24 +0200

    teamTest

--- src/TeamTest.java
+++ src/TeamTest.java
@@ -19,12 +19,12 @@ public class TeamTest {
 
     Team team1;
 
-    Spieler reitermaniac;
-    Spieler whiteWolf;
-    Spieler bloodyMary;
-    Spieler sonic;
-    Spieler hotShotGG;
-    Spieler cornisier;
+    Spieler s1;
+    Spieler s2;
+    Spieler s3;
+    Spieler s4;
+    Spieler s5;
+    Spieler s6;
 
 
     @Before
@@ -34,55 +34,55 @@ public class TeamTest {
 
         team1 = new Team("Beamoforce One");
 
-        reitermaniac = new Spieler();
-        whiteWolf = new Spieler();
-        bloodyMary = new Spieler();
-        sonic = new Spieler();
-        hotShotGG = new Spieler();
-        cornisier = new Spieler();
+        s1=new Spieler("Anna",25,"BloodyMary","Angriff","aktiv");
+        s2=new Spieler("Franz",30,"WhiteWolf","Mittelfeld","aktiv");
+        s3=new Spieler("Tom",20,"Reitermaniac","Angriff","aktiv");
+        s4=new Spieler("Martin",28,"Unreality","Angriff","aktiv");
+        s5=new Spieler("Werner",35,"Horsti","Mittelfeld","aktiv");
+        s6=new Spieler("Chris",23,"GreyWarden","Verteidigung","aktiv");
 
-        team1.addToTeam(reitermaniac);
-        team1.addToTeam(whiteWolf);
-        team1.addToTeam(bloodyMary);
-        team1.addToTeam(sonic);
-        team1.addToTeam(hotShotGG);
-        team1.addToTeam(cornisier);
+        team1.addToTeam(s1);
+        team1.addToTeam(s2);
+        team1.addToTeam(s3);
+        team1.addToTeam(s4);
+        team1.addToTeam(s5);
+        team1.addToTeam(s6);
 
-        reitermaniac.updateScore(16,5); //Score: 65
-        whiteWolf.updateScore(12,6);    //Score: 42
-        bloodyMary.updateScore(14,9);   //Score: 43
-        sonic.updateScore(27,16);       //Score: 87
-        hotShotGG.updateScore(19,8);    //Score: 71
-        cornisier.updateScore(12,14);   //Score: 18
+        s1.updateScore(180);       //Score: 65
+        s2.updateScore(120);       //Score: 42
+        s3.updateScore(100);       //Score: 43
+        s4.updateScore(50);      //Score: 87
+        s5.updateScore(50);       //Score: 71
+        s6.updateScore(100);      //Score: 18
                                         //SumScore: 326
-        reitermaniac.getScore();
-        whiteWolf.getScore();
-        bloodyMary.getScore();
-        sonic.getScore();
-        hotShotGG.getScore();
-        cornisier.getScore();
+        s1.getScore();
+        s2.getScore();
+        s3.getScore();
+        s4.getScore();
+        s5.getScore();
+        s6.getScore();
 
     }
 
     @Test
     public void testAddToTeam() throws Exception {
-        assertEquals("Erwartet Reitermaniac",team1.getSpieler()[0],reitermaniac);
-        assertEquals("Erwartet White Wholf",team1.getSpieler()[1],whiteWolf);
-        assertEquals("Erwartet Bloody Mary", team1.getSpieler()[2],bloodyMary);
-        assertEquals("Erwartet Sonic", team1.getSpieler()[3],sonic);
-        assertEquals("Erwartet HotShotGG", team1.getSpieler()[4],hotShotGG);
-        assertEquals("Erwartet Cornisier", team1.getSpieler()[5],cornisier);
+        assertEquals("Erwartet Reitermaniac",team1.getSpieler()[0],s1);
+        assertEquals("Erwartet White Wholf",team1.getSpieler()[1],s2);
+        assertEquals("Erwartet Bloody Mary", team1.getSpieler()[2],s3);
+        assertEquals("Erwartet Sonic", team1.getSpieler()[3],s4);
+        assertEquals("Erwartet HotShotGG", team1.getSpieler()[4],s5);
+        assertEquals("Erwartet Cornisier", team1.getSpieler()[5],s6);
         assertEquals("Spieleranzahl = 6",team1.getTeamMember(),6);
     }
 
     @Test
     public void testRemoveFromTeam() throws Exception {
-        team1.removeFromTeam(reitermaniac, team1);
-        team1.removeFromTeam(sonic, team1);
+        team1.removeFromTeam(s1, team1);
+        team1.removeFromTeam(s4, team1);
         assertEquals("Anzahl Teammitglieder = 4",team1.getTeamMember(),4);
         assertEquals("Anzahl Teamlose Spieler = 2", team1.teamloseSpieler.size(),2);
-        assertEquals("Erwartet Reitermaniac", team1.teamloseSpieler.get(0),reitermaniac);
-        assertEquals("Erwartet Sonic", team1.teamloseSpieler.get(1),sonic);
+        assertEquals("Erwartet Reitermaniac", team1.teamloseSpieler.get(0),s1);
+        assertEquals("Erwartet Sonic", team1.teamloseSpieler.get(1),s4);
     }
 
     @Test
@@ -99,7 +99,7 @@ public class TeamTest {
             players[i] = team1.getSpieler()[i];
         }*/
         team1.sumScore();
-        assertEquals("Score = 326", team1.getTeamScore(),326);
+        assertEquals("Score = 500", team1.getTeamScore(),326);
     }
 
     @Test
